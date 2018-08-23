ecc83960a12bc4614a6d9c9f8cd964f038d6046c
pvalderama <34151463+pvalderama@users.noreply.github.com>
2018-08-23 12:25:44 -0700

    oAuth

---
+++ client/src/components/auth/RequireAuth.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default (ChildComponent, auth) => {
+  class ComposedComponent extends Component {
+    componentDidMount(){
+      this.shouldNavigateAway();
+    }
+
+    componentDidUpdate(){
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if(!auth){
+        this.props.history.push('/');
+      }
+    }
+
+    render(){
+      return <ChildComponent {...this.props} />
+    }
+  }
+
+  return ComposedComponent;
+};
--- server/auth_routes.js
+++ server/auth_routes.js
@@ -1,3 +1,26 @@
-module.exports = (app, pool) => {
-    console.log("at auth")
-}
+const passport = require('passport');
+
+module.exports = app => {
+    app.get(
+        '/auth/google',
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+    );
+
+    app.get('/auth/google/callback',
+        passport.authenticate('google'), 
+        (req, res) => {
+            res.redirect('/shop')
+        }
+    );
+
+    app.get('/api/logout', (req, res) => {
+        req.logout(); //logout added by passport
+        res.redirect('/');
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user) //req.user generated from cookie session and passport
+    });
+};
