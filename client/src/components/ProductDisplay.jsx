992f9ff514aec4bf33a6a14b3b984eff38516635
Clariz Mariano <clariz.mariano@gmail.com>
2018-08-23 19:36:45 -0700

    added some stylin'

--- client/src/components/ProductDisplay.jsx
+++ client/src/components/ProductDisplay.jsx
@@ -5,7 +5,9 @@ const ProductDisplay = (props) => {
 
     <div className="productBox">
       <div><a href="/shop/item"><img src={props.photo} alt="product"></img></a></div>
-      <div>{props.description}</div>
+      <div>
+        <h4>{props.title}</h4>
+        <p>{props.description}</p></div>
       <button>Add to Cart</button>
     </div>
   );
